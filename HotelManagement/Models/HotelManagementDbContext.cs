45deb348137e78fb34bdd3cb664d121338434df0
Robert <robertodhiambo661@gmail.com>
2023-07-28 19:17:16 +0300

    dbContext/ connectionString class commit

---
+++ HotelManagement/Models/HotelManagementDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Models
+{
+    public class HotelManagementDbContext : DbContext
+    {
+        public HotelManagementDbContext(DbContextOptions<HotelManagementDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<RoomTypePricing> RoomTypePricings { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating ( ModelBuilder modelBuilder )
+        {
+            base.OnModelCreating ( modelBuilder );
+        }
+    }
+
+}
--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -1,7 +1,14 @@
+using HotelManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder ( args );
 
 // Add services to the container.
 builder.Services.AddControllersWithViews ( );
+
+builder.Services.AddDbContext<HotelManagementDbContext> ( options =>
+    options.UseSqlServer ( builder.Configuration.GetConnectionString ( "DefaultConnection" ) ));
 
 var app = builder.Build ( );
 
